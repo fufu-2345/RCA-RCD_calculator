a53d22026fce8ec84b5a0f8216196699b0e2994d
fufu-2345 <mr.kittisak.ploysumrit@gmail.com>
2025-07-19 19:04:44 +0700

    done read csv

--- apps/back/main.py
+++ apps/back/main.py
@@ -14,11 +14,29 @@ app.add_middleware(
 )
 @app.get("/")
 def root():
-    return {"message": "Welcome to the FastAPI application!"}
+    filepath = f"./data/world/World.csv"
+    if not os.path.exists(filepath):
+        return {"error": "File not found"}
+    
+    df = pd.read_csv(filepath)
+    print("a")
+    return df.to_dict(orient="records")
+    #return {"message": "Welcome to the FastAPI application!"}
+    
+@app.get("/get_folders")
+def list_folders():
+    base_path = "./data"
+    try:
+        folders = [name for name in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, name))]
+        print(folders)
+        return {"folders": folders}
+    except Exception as e:
+        return {"error": str(e)}
 
 @app.get("/read_csv")
 def read_csv(filename: str = Query(...)):
-    filepath = f"./csv/{filename}"
+    print("./data/"+filename+"/"+filename+".csv")
+    filepath = f"./data/{filename}/{filename}.csv"
     
     if not os.path.exists(filepath):
         return {"error": "File not found"}
--- apps/front/app/page.tsx
+++ apps/front/app/page.tsx
@@ -1,9 +1,44 @@
-export default function Page() {
+"use client";
+
+import React, { useState, useEffect } from 'react';
+
+function Page() {
+  const [filename, setFilename] = useState('');
+  const [data, setData] = useState([]);
+  const [folders, setFolders] = useState([]);
+
+  useEffect(() => {
+    const fetchFolders = async () => {
+      const response = await fetch("http://localhost:8000/get_folders");
+      const json = await response.json();
+      if (json.folders) {
+        setFolders(json.folders);
+      }
+    };
+    fetchFolders();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await fetch(`http://localhost:8000/read_csv?filename=${filename}`);
+    const json = await response.json();
+    setData(json);
+  };
+
   return (
-    <main>
-      <div>
-        main
-      </div>
-    </main>
+    <div>
+      <label>select: </label>
+      <select value={filename} onChange={e => setFilename(e.target.value)}>
+        <option value="">-- select the ... --</option>
+        {folders.map((folder, index) => (
+          <option key={index} value={folder}>
+            {folder}
+          </option>
+        ))}
+      </select>
+      <button onClick={fetchData}>โหลดไฟล์</button>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
   );
 }
+
+export default Page;
